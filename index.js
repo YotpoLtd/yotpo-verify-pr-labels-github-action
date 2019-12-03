c1ce0a0b1b397603728694c1de73d3b1bfc81e38
Rachel Guetta <rguetta@yotpo.com>
2019-12-03 18:31:44 +0100

    --amend

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const requiredLabels = core.getInput('requiredLabels');
-console.log('required labels are: ' + requiredLabels)
+const commaSeparatedRequiredLabels = core.getInput('requiredLabels');
 
-const labels = github.context.payload.pull_request.labels
-const labelNames = labels.map(label => label.name)
-console.log('labels on this pull request are: ' + labelNames)
-
-if (!(requiredLabels.some(requiredLabel => labelNames.includes(requiredLabel)))) {
-    core.setFailed('Your pull request should contain one of the required labels: ' + requiredLabels);
+if (commaSeparatedRequiredLabels) {
+    const requiredLabels = commaSeparatedRequiredLabels.split(',')
+    console.log('required labels are: ' + requiredLabels)
+    
+    const labels = github.context.payload.pull_request.labels
+    const labelNames = labels.map(label => label.name)
+    console.log('labels on this pull request are: ' + labelNames)
+    
+    if (!(requiredLabels.some(requiredLabel => labelNames.includes(requiredLabel)))) {
+        core.setFailed('Your pull request should contain one of the required labels: ' + requiredLabels);
+    }
 }
